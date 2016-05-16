a6fc75e50005d818b0864cd4482f4e486563ffff
CJ-Wright <cjwright4242@gmail.com>
2016-05-16 09:47:17 -0400

    ENH: new resolution generator

--- pdf/figures/scripts/res_gen.py
+++ pdf/figures/scripts/res_gen.py
@@ -1,24 +1,31 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as sts
+from pyFAI.geometry import Geometry
+from skbeam.core.recip import generate_q_bins
 from skbeam.core.utils import twotheta_to_q
-import matplotlib
 
 plt.style.use('/mnt/bulk-data/Masters_Thesis/config/thesis.mplstyle')
 
 save_file = '/mnt/bulk-data/Masters_Thesis/pdf/figures/'
 
-n_pixels = 1048
-size_pixels = .0002
-pixels = np.linspace(0, n_pixels * size_pixels, n_pixels)
-dist = .25
+geo = Geometry(
+    detector='Perkin', pixel1=.0002, pixel2=.0002,
+    dist=.23,
+    poni1=.209, poni2=.207,
+    # rot1=.0128, rot2=-.015, rot3=-5.2e-8,
+    wavelength=1.43e-11
+)
 
-pixel_bottom = pixels
-pixel_top = pixels + size_pixels
-tthb = np.arctan(pixel_bottom / dist)
-ttht = np.arctan(pixel_top / dist)
-dq = twotheta_to_q(ttht, .143) - twotheta_to_q(tthb, .143)
+r = geo.rArray((2048, 2048))
+q = geo.qArray((2048, 2048)) / 10
+bins = generate_q_bins(np.max(r)-.5*geo.pixel1, geo.pixel1, geo.dist, .143)
 
-plt.plot(twotheta_to_q(pixels/dist, .143), dq)
+res = []
+for i in range(len(bins) - 1):
+    res.append(bins[i+1] - bins[i])
+
+plt.plot(bins[:-1], res)
 # plt.title(r'$Q$ resolution')
 plt.ylabel(r'$\Delta Q (\AA^{-1}$)')
 plt.xlabel(r'$Q (\AA^{-1}$)')
