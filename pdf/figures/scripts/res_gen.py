1eaff91efc7651b5a5c39f6371f018f0ab2d15b9
CJ-Wright <cjwright4242@gmail.com>
2016-05-16 14:21:25 -0400

    ENH: added the integration workflow plots

--- pdf/figures/scripts/res_gen.py
+++ pdf/figures/scripts/res_gen.py
@@ -21,18 +21,37 @@ r = geo.rArray((2048, 2048))
 q = geo.qArray((2048, 2048)) / 10
 bins = generate_q_bins(np.max(r)-.5*geo.pixel1, geo.pixel1, geo.dist, .143)
 
+int_q = np.zeros(q.shape, dtype=np.int)
+for i in range(len(bins)-1):
+    t_array = (bins[i] <= q) & (q < bins[i+1])
+    int_q[t_array] = i
+
 res = []
 for i in range(len(bins) - 1):
     res.append(bins[i+1] - bins[i])
 
-plt.plot(bins[:-1], res)
-# plt.title(r'$Q$ resolution')
+counts = sts.binned_statistic(q.ravel(), int_q.ravel(), bins=bins, statistic='count')[0]
+
+even_bins_bins = np.linspace(0, np.max(q), 2000)
+even_bins_res = []
+for i in range(len(even_bins_bins) - 1):
+    even_bins_res.append(even_bins_bins[i+1] - even_bins_bins[i])
+
+plt.plot(bins[:-1], res, label=r'$Q resolution binning $')
+plt.plot(even_bins_bins, even_bins_res, label=r'$Even binning$')
 plt.ylabel(r'$\Delta Q (\AA^{-1}$)')
 plt.xlabel(r'$Q (\AA^{-1}$)')
+plt.legend()
 plt.tight_layout()
-plt.savefig(save_file + 'res.eps', bbox_inches='tight',
-            transparent='True')
-plt.savefig(save_file + 'res.png', bbox_inches='tight',
-            transparent='True')
-plt.savefig(save_file + 'res.pdf', bbox_inches='tight',
-            transparent='True')
+for end in ['eps', 'png', 'pdf']:
+    plt.savefig(save_file + 'res.{}'.format(end), bbox_inches='tight',
+                transparent='True')
+
+plt.clf()
+plt.plot(bins[:-1], counts)
+plt.ylabel('Number of Pixels')
+plt.xlabel(r'$Q (\AA^{-1}$)')
+plt.tight_layout()
+for end in ['eps', 'png', 'pdf']:
+    plt.savefig(save_file + 'pixels.{}'.format(end), bbox_inches='tight',
+                transparent='True')
